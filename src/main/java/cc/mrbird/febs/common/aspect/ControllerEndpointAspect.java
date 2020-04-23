3742f9c74fccc26528600c8bc958657463be50d8
jht385 <251064029@qq.com>
2020-04-23 14:06:01 +0800

    更新到 Apr 23, 2020

--- src/main/java/cc/mrbird/febs/common/aspect/ControllerEndpointAspect.java
+++ src/main/java/cc/mrbird/febs/common/aspect/ControllerEndpointAspect.java
@@ -17,10 +17,12 @@ import cc.mrbird.febs.common.exception.FebsException;
 import cc.mrbird.febs.common.utils.FebsUtil;
 import cc.mrbird.febs.monitor.service.ILogService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author MrBird
  */
+@Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
@@ -52,6 +54,7 @@ public class ControllerEndpointAspect extends BaseAspectSupport {
 			}
 			return result;
 		} catch (Throwable throwable) {
+			log.error(throwable.getMessage(), throwable);
 			String exceptionMessage = annotation.exceptionMessage();
 			String message = throwable.getMessage();
 			String error = FebsUtil.containChinese(message) ? exceptionMessage + "，" + message : exceptionMessage;
